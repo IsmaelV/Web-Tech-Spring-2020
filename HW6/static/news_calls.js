f88c10746b45af1f6b7000ee4e62f077578c88e6
IsmaelV <iv2181@columbia.edu>
2020-03-04 11:08:26 -0800

    Fixed bug for word cloud on firefox

--- HW6/static/news_calls.js
+++ HW6/static/news_calls.js
@@ -187,7 +187,7 @@ function write_word_cloud(top_headlines) {
 	      .selectAll("text")
 	        .data(words)
 	      .enter().append("text")
-	        .style("font-size", function(d) { return d.size; })
+	        .style("font-size", function(d) { return d.size + "px"; })
 	        .style("fill", "#69b3a2")
 	        .attr("text-anchor", "middle")
 	        .style("font-family", "Impact")
