3c32c96e3027a1aa6815c5d3b2a28ab9d55d612a
IsmaelV <iv2181@columbia.edu>
2020-04-20 17:44:20 -0700

    Restrict number of articles for NY Times to 10 on section page

--- HW8/hw8-webtech-news/client/src/components/allSectionArticles.jsx
+++ HW8/hw8-webtech-news/client/src/components/allSectionArticles.jsx
@@ -17,8 +17,22 @@ class AllSectionArticles extends Component {
       // for (var key in Object.keys(this.props.articles.results)) {  // This is to remove error of no key in console
       //   this.props.articles.results.key.key = String(key);
       // }
+      var nytimesArticles = [];
+      for (var i = 0; i < this.props.articles.results.length; i++) {
+        if (nytimesArticles.length === 10) {
+          break;
+        }
+        if (
+          this.props.articles.results[i].section &&
+          this.props.articles.results[i].title &&
+          this.props.articles.results[i].published_date &&
+          this.props.articles.results[i].abstract
+        ) {
+          nytimesArticles.push(this.props.articles.results[i]);
+        }
+      }
       this.state = {
-        all_articles: this.props.articles.results,
+        all_articles: nytimesArticles,
         news: "nytimes"
       };
     }
